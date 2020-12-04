fb36f88fecee4f3ff4a31890328421343c81fad5
cmarro3 <christophermarroquin352@gmail.com>
2020-12-04 15:06:35 -0600

    update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include<vector>
 using namespace std;
 
 struct Node {
@@ -25,54 +25,54 @@ public:
     void insert(int value);
 
     TernaryTree() {
-        root = NULL;
+        root = nullptr;
     }
 };
 void TernaryTree::insert_h(int value, Node *leaf) {
     if(value < leaf->data) {
-        if(leaf->left != NULL)
+        if(leaf->left != nullptr)
             insert_h(value, leaf->left);
         else {
             leaf->left = new Node;
             leaf->left->data = value;
-            leaf->left->left = NULL;
-            leaf->left->right = NULL;
-            leaf->middle->middle = NULL;
+            leaf->left->left = nullptr;
+            leaf->left->right = nullptr;
+            leaf->middle->middle = nullptr;
         }
     }
     else if (value == leaf -> data){
-        if(leaf ->middle != NULL)
+        if(leaf ->middle != nullptr)
             insert_h(value, leaf ->middle);
         else {
             leaf ->middle = new Node;
             leaf ->middle->data = value;
-            leaf->left->left = NULL;
-            leaf->left->right = NULL;
-            leaf->middle->middle = NULL;
+            leaf->left->left = nullptr;
+            leaf->left->right = nullptr;
+            leaf->middle->middle = nullptr;
         }
 
     }
     else if(value > leaf->data) {
-        if(leaf->right != NULL)
+        if(leaf->right != nullptr)
             insert_h(value, leaf->right);
         else {
             leaf->right = new Node;
             leaf->right->data = value;
-            leaf->right->left = NULL;
-            leaf->right->right= NULL;
-            leaf->middle->middle = NULL;
+            leaf->right->left = nullptr;
+            leaf->right->right= nullptr;
+            leaf->middle->middle = nullptr;
         }
     }
 }
 void TernaryTree::insert(int value){
-    if(root != NULL)
+    if(root != nullptr)
         insert_h(value, root);
     else {
         root = new Node;
         root->data = value;
-        root->left = NULL;
-        root->right = NULL;
-        root->middle = NULL;
+        root->left = nullptr;
+        root->right = nullptr;
+        root->middle = nullptr;
 
     }
 }
@@ -84,21 +84,22 @@ void TernaryTree::construct_tree(int A[], int n) {
 }
 
 void TernaryTree::traverse_LMRW_h(Node *current) {
-    if (current != NULL) {
+    if (current != nullptr) {
         traverse_LMRW_h(current->left);
         traverse_LMRW_h(current->right);
-        traverse_LMRW_h(current ->middle);
+        traverse_LMRW_h(current->middle);
         cout<< current-> data << endl;
     }
 }
+
 void TernaryTree::traverse_LMRW(){
     return traverse_LMRW_h(root);
 }
 
 int TernaryTree::leaf_count_h(Node *current){
     static int cnt = 0;
-    if (current != NULL) {
-        if ((current->left == NULL) and (current->right == NULL))
+    if (current != nullptr) {
+        if ((current->left == nullptr) and (current->right == nullptr))
             cnt += 1;
         leaf_count_h(current->left);
         leaf_count_h(current->right);
@@ -112,7 +113,7 @@ int TernaryTree::leaf_count() {
 }
 int TernaryTree::degree_one_nodes_count_h(Node *current){
     static int sum = 0;
-    if (current != NULL) {
+    if (current != nullptr) {
         sum += current->data;
         degree_one_nodes_count_h(current->left);
         degree_one_nodes_count_h(current->right);
